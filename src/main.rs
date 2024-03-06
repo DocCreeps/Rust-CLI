3c7371ee6c3b217946ace5b0fb821674b6790829
DocCreeps <dorian1998dino@hotmail.com>
2024-03-06 10:34:30 +0100

    add commentaire

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,36 @@ use std::io::{self, Write};
 use std::collections::HashMap;
 
 fn main() {
+    // Initialisation des variables de statistiques
     let mut victories = 0;
     let mut defeats = 0;
     let mut total_attempts = 0;
     let mut attempts_counter: HashMap<u32, u32> = HashMap::new();
 
+    // Boucle principale du jeu
     loop {
+        // Génération d'un nouveau nombre secret entre 0 et 1000
         let secret_number: u32 = rand::random::<u32>() % 1001;
         let mut attempts = 0;
 
         println!("Nouvelle partie !");
 
+        // Boucle du jeu
         loop {
+            // Vérification du nombre d'essais
             if attempts >= 10 {
                 println!("Désolé, vous avez atteint la limite de {} essais. Le chiffre secret était : {}", attempts, secret_number);
                 defeats += 1;
                 break;
             }
 
+            // Lecture de l'entrée utilisateur
             let mut guess = String::new();
-
             print!("Devinez le chiffre : ");
             io::stdout().flush().unwrap();
             io::stdin().read_line(&mut guess).expect("Erreur lors de la lecture de l'entrée.");
 
+            // Conversion de l'entrée en u32
             let guess: u32 = match guess.trim().parse() {
                 Ok(num) => num,
                 Err(_) => {
@@ -34,9 +40,11 @@ fn main() {
                 }
             };
 
+            // Incrémentation du nombre d'essais
             attempts += 1;
             total_attempts += 1;
 
+            // Vérification du guess par rapport au nombre secret
             if guess == secret_number {
                 println!("Bravo ! Vous avez deviné le chiffre secret en {} essai(s).", attempts);
                 victories += 1;
@@ -59,8 +67,8 @@ fn main() {
             println!(" {} essai(s) : {} victoire(s)", attempts, wins);
         }
 
+        // Demande à l'utilisateur s'il souhaite jouer à nouveau
         let mut play_again = String::new();
-
         print!("Voulez-vous jouer à nouveau ? (O/N) : ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut play_again).expect("Erreur lors de la lecture de l'entrée.");
